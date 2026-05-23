ffda7e0d8b0f6cffb571e4ff913791efa5ff2849
keerthika.r <keerthikakiki2004@gmail.com>
2026-05-23 07:39:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+@app.route('/')
+def home():
+    posts = Blog.query.all()
+    return render_template('home.html', posts=posts)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        new_post = Blog(title=title, content=content)
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/')
+
+    return render_template('create.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    post = Blog.query.get_or_404(id)
+
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+
+        db.session.commit()
+
+        return redirect('/')
+
+    return render_template('edit.html', post=post)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Blog.query.get_or_404(id)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect('/')
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
